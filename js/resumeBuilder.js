b7427b29aeb93dc1f13435748f27960f74ff69d8
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2016-11-25 10:22:24 -0500

    Add formatted name and role to resumebuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
+var name = "Jonathan Fitzgibbon";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
